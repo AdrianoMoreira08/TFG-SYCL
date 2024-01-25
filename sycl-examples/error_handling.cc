06de0b95e0e70dcf03edcda820006be77673a543
AdrianoMoreira08 <alu0101436784@ull.edu.es>
2024-01-25 15:36:42 +0000

    Error handling example

---
+++ sycl-examples/error_handling.cc
@@ -0,0 +1,26 @@
+/**
+ * Universidad de La Laguna. Escuela Superior de Ingenierı́a y Tecnologı́a
+ * Grado en Ingenierı́a Informática
+ * @author Adriano dos Santos Moreira <alu0101436784@ull.edu.es>
+ * @brief Handling of synchronous and asynchronous errors example.
+ */
+
+#include <sycl/sycl.hpp>
+#include <iostream>
+
+auto AsyncHandler = [](sycl::exception_list exceptions) {
+  for (auto& exception: exceptions) {
+    std::rethrow_exception(exception);
+  }
+};
+
+int main() {
+  try {
+    sycl::queue queue{AsyncHandler};
+    // Potentially exception-prone code
+    // ...
+  } catch (sycl::exception& e) {
+    std::cerr << "SYCL exception has occurred.\n"
+              << e.what() << std::endl;
+  }
+}
